a1169dc6bf4dad03af76ba2d94121e443a21a0c1
purushottam8208758977 <puruissimple@gmail.com>
2019-11-04 16:14:48 +0530

    notes done

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -14,15 +14,11 @@ import { deepOrange, deepPurple } from '@material-ui/core/colors';
 import Grid from '@material-ui/core/Grid';
 import MenuIcon from '@material-ui/icons/Menu';
 
-import Drawer from '@material-ui/core/Drawer';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
 
 //child components
 import { TakeNote } from './TakeNote'
 import { Display } from './display'
+import {DrawerMade} from './drawer'
 /**
  * @description - This prop is a inbuilt prop we are modifying it
  */
@@ -256,28 +252,7 @@ export class Dashboard extends Component {
                             <Avatar className={this.classes.orangeAvatar}>N</Avatar>
                         </Grid></div>
                     </Card>
-                    <Drawer
-                        className={this.classes.drawer}
-                        variant="persistent"
-                        anchor="left"
-                        open={this.state.open} //opens only when rhs true
-                        classes={{
-                            paper: this.classes.drawerPaper,
-                        }}
-                    >
-                        <ListItem button key="Notes" onClick={this.loadNotes}>
-                            <ListItemIcon>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="gb_Rc"><path d="M9 21c0 .55.45 1 1 1h4c.55 0 1-.45 1-1v-1H9v1zm3-19C8.14 2 5 5.14 5 9c0 2.38 1.19 4.47 3 5.74V17c0 .55.45 1 1 1h6c.55 0 1-.45 1-1v-2.26c1.81-1.27 3-3.36 3-5.74 0-3.86-3.14-7-7-7zm2.85 11.1l-.85.6V16h-4v-2.3l-.85-.6A4.997 4.997 0 0 1 7 9c0-2.76 2.24-5 5-5s5 2.24 5 5c0 1.63-.8 3.16-2.15 4.1z"></path></svg>
-                                <ListItemText className="DrawerText" primary="Notes" />
-                            </ListItemIcon>
-                        </ListItem>
-
-                        <ListItem button key="Reminders">
-                            <ListItemIcon>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="gb_Rc"><path d="M18 17v-6c0-3.07-1.63-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.64 5.36 6 7.92 6 11v6H4v2h16v-2h-2zm-2 0H8v-6c0-2.48 1.51-4.5 4-4.5s4 2.02 4 4.5v6zm-4 5c1.1 0 2-.9 2-2h-4c0 1.1.9 2 2 2z"></path></svg>                                <ListItemText className="DrawerText" primary="Reminders" />
-                            </ListItemIcon>
-                        </ListItem>
-                    </Drawer>
+                   <Drawer />
                     <div id="Two">
                         <TakeNote refresh={this.NoteDisplay} />
                         <Display ref={this.CreatingNoteInstance}/>
