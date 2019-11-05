667790c19c2ea541d2c21834329c2b23c44ae9a0
purushottam8208758977 <puruissimple@gmail.com>
2019-11-05 20:12:11 +0530

    all arrays displayed

--- src/components/oneTrashNote.jsx
+++ src/components/oneTrashNote.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Card from '@material-ui/core/Card';
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import Button from '@material-ui/core/Button';
+
 export class OneTrashNote extends Component {
 
     handleRefresh=()=>{
@@ -12,7 +14,7 @@ export class OneTrashNote extends Component {
                 <Card id="NoteDimensions">
                    {this.props.data.title }<br/>
                    {this.props.data.description}
-                   <div><DeleteForeverIcon /></div>
+                   <Button id="deleteButton"><img id="deleteForever" src={require('../assets/deleteforever.png')} alt="delete pic"></img> </Button>
                 </Card>
                 </div>
             
