3c091c940180c8621224a307b27d08fdb7b84958
purushottam8208758977 <puruissimple@gmail.com>
2019-11-06 12:31:08 +0530

    comments

--- src/components/dialogNote.jsx
+++ src/components/dialogNote.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
-import { IconsList } from './iconsList';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 
 export class DialogNote extends Component {
@@ -10,15 +10,18 @@ export class DialogNote extends Component {
     render() {
         return (
             <div>
-                <Card  onClick={this.handleClickOpen}>
+                <Card onClick={this.handleClickOpen}>
                     <div id="dialog">{this.props.dialogData.title}<br />
-                    {this.props.dialogData.description}</div>
-                    <IconsList individualNoteData={this.props.data} refreshingAfterTrashing={this.handleRefresh} />
-                    <Button onClick={this.props.closeDialog} >
-                        Close
-                    </Button>
-                </Card>
-            </div>
+                        {this.props.dialogData.description}</div>
+                    <div id="Icons">
+                        <Button><img src={require('../assets/reminder.svg')} alt="reminder pic"></img> </Button>
+                        <Button> <img src={require('../assets/pallete.svg')} alt="pallete pic"></img>  </Button>
+                        <Button> <img src={require('../assets/archive.svg')} alt="archive pic "></img> </Button>
+                        <Button onClick={(event) => this.handleMenu(event)}><MoreVertIcon></MoreVertIcon></Button>
+                    
+                    <Button onClick={this.props.closeDialog} ><b>close</b></Button></div>
+                </Card >
+            </div >
         );
     }
 }
