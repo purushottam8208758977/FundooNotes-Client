a4d64d59e5499fcfc04df96252e70a3e74b847c2
purushottam8208758977 <puruissimple@gmail.com>
2019-11-07 19:18:38 +0530

    coloring and restoring done !

--- src/components/oneTrashNote.jsx
+++ src/components/oneTrashNote.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Card from '@material-ui/core/Card';
 import IconButton from '@material-ui/core/IconButton';
-import {deleteNote} from '../services/services'
+import {deleteNote,updateNote} from '../services/services'
 import Tooltip from '@material-ui/core/Tooltip';
 
 export class OneTrashNote extends Component {
@@ -20,16 +20,32 @@ export class OneTrashNote extends Component {
         deleteNote(deleteObject).then((responseReceived) => {
             console.log("\n\n\t one Trash note --> deleting note response--->", responseReceived)
             this.props.refreshDisplay()
+            this.props.refreshNotes()
+
         })
     }
 
+    restoringNote=()=>{
+        //initiating restoring process
+        let restoreObject={}
+        restoreObject.noteId=this.props.data._id
+        restoreObject.updating={trash:false}
+        updateNote(restoreObject).then((responseOfRestoringNote)=>{
+            console.log("\n\n\tResponse of --> restoring-->",responseOfRestoringNote)
+            this.props.refreshDisplay()
+            this.props.refreshNotes()
+
+        })
+    }
     render() {
         return (
                 <div id="NotesReceived">
-                <Card id="NoteDimensions">
+                <Card id="NoteDimensions" style={{backgroundColor:this.props.data.color}}>
                    {this.props.data.title }<br/>
                    {this.props.data.description}
                    <Tooltip title="Delete Forever"><IconButton onClick={this.deleteNoteForever} id="deleteButton"><img id="deleteForever" src={require('../assets/deleteforever.png')} alt="delete pic"></img> </IconButton></Tooltip>
+                   <Tooltip title="Restore"><IconButton onClick={this.restoringNote} ><img src={require('../assets/restore.svg')} alt="restore pic"></img> </IconButton></Tooltip>
+
                 </Card>
                 </div>
             
