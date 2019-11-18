9ef4e32d8c0879d121a3e5ad8ed056c1a0c60115
purushottam8208758977 <puruissimple@gmail.com>
2019-11-18 13:29:40 +0530

    JEST testing done

--- src/components/login.test.js
+++ src/components/login.test.js
@@ -1,25 +1,38 @@
 import React from 'react'
-import Enzyme, { shallow,mount } from 'enzyme'
+import Enzyme, { shallow, mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import { Login } from './login'                     //this component will be tested
 import '../Login.css'
-
+import { expect } from 'chai';
 
 
 
 Enzyme.configure({ adapter: new Adapter() })
 
-describe("checking component",()=>{
-    let component = mount(<Login/>)
+describe("checking component", () => {
+    let component = mount(<Login />)
 
-    it("Checking the presence of whole component",()=>{
+    it("Checking the presence of whole component", () => {
         component.debug() // passes only when the component exists
-        component.contains(  <div className="SignIn"> <br /> Sign in </div>)
-    
-        console.log("\n\n\ttest result--> ",component.debug())
+        component.contains(<div className="SignIn"> <br /> Sign in </div>)
+        //        console.log("\n\n\ttest result--> ",component.debug())
     })
 
-    // it("sdds",()=>{
-    //     component.first()
-    // })
+})
+
+describe('Login component', () => {
+    it('Checks the class U instances', () => {
+        const wrapper = shallow(<Login />);
+        expect(wrapper.find('.U')).to.have.lengthOf(3);
+    })
+    it('Checking number of constructors ', () => {
+        const wrapper = mount(<Login />);
+        expect(wrapper.find(Login)).to.have.lengthOf(1);
+    })
+    
+    it('Existence of a CSS class',()=>{
+    const wrapper = mount(<div className="Login" />);
+    expect(wrapper.exists('.Login')).to.equal(true);
+    expect(wrapper.find('.other-class').exists()).to.equal(false);
+    })
 })
